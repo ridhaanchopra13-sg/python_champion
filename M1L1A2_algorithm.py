4fee795c0c90748a2e5dfd21cdd199c65c9a8592
ridhaanchopra13-sg <ridhaanchopra13@gmail.com>
2026-01-09 21:33:28 +0800

    LESSON 1 ACTIVITIES

---
+++ M1L1A2_algorithm.py
@@ -0,0 +1,15 @@
+"""Write a algorithm to to find sum of two numbers
+
+1. Start
+
+2. Declare three variables num1 , num2 and sum
+
+3. Read the values of num1 and num2
+
+4. Add num1 and num2 and store the result in sum.
+
+sum= num1 + num2
+
+5. Print the value of sum
+
+6. End"""
