2a18452abc326e569e1f09e61939a4dd3a3a2b9b
ridhaanchopra13-sg <ridhaanchopra13@gmail.com>
2026-01-16 21:29:04 +0800

    Add files via upload

---
+++ M1L2A1_printargument.py
@@ -0,0 +1,24 @@
+
+#Print a Sentence
+
+print("Welcome to the world of programming!")
+
+#Print a Number
+
+print(10.0)
+
+#Next Line after print
+
+print("Hello Python \n")
+
+#Pass multiple arguments for printing
+
+print("hello",5)
+
+#End argument of Print Statement
+
+print("welcome to ", end= "\n Hola Soy Espanol\n")
+
+#Print with variables fstring
+
+print(f"Hello, I am learning Python version {6+8}")
---
+++ M1L2A2_variables.py
@@ -0,0 +1,8 @@
+Z = 5
+name = "Ridhaan"
+print(Z)
+print(name)
+user = input("Enter name\n")
+print(user)
+codingal = "Codingal"
+print("Welcome to",codingal)
