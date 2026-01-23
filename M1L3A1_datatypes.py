19ae432ee67f51c4cab191c241bd8228ce979063
ridhaanchopra13-sg <ridhaanchopra13@gmail.com>
2026-01-23 21:29:37 +0800

    Add files via upload

---
+++ M1L3A1_datatypes.py
@@ -0,0 +1,8 @@
+a = 5
+print("Data type of A is",type(a))
+b = 2.5
+print("Data type of B is",type(b))
+c = "HUMAN"
+print("Data type of C is",type(c))
+d = True
+print("Data type of D is",type(d))
---
+++ M1L3A3_stringoperation1.py
@@ -0,0 +1,18 @@
+#input a word
+word = str(input("Enter a string: "))
+#Reverse String
+#using step value as 1 to iterate in reverse
+revword = word[::-1]
+text = revword
+print("Reverse of Given String is:")
+print(text)
+str1="Hello"
+str2 ="World"
+print("String Operations:")
+print(len(str1))
+print(str1 + str2)
+print(str1.upper())
+print(str2.lower())
+print(str1.replace("H","h"))
+print(str2.split("o"))
+print(str1[2])
