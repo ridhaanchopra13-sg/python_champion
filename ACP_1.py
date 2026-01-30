ce7501529a093d77db5603edd537e67f0afe78c1
ridhaanchopra13-sg <ridhaanchopra13@gmail.com>
2026-01-30 10:22:01 +0800

    Add files via upload

---
+++ ACP_1.py
@@ -0,0 +1,16 @@
+birthdays = {
+    "Alice": "23/7/13",
+    "Josh": "5/11/14",
+    "Sam": "15/10/13",
+    "Zack": "2/5/12",
+    "Joe": "27/6/13"
+}
+
+name = input("Enter name: ")
+
+print(birthdays[name])
+
+
+
+
+#it can only be Alice not alice
