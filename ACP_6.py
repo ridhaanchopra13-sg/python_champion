2c94c43debf81fc0a3e4b9cbaf886ebc1bace414
ridhaanchopra13-sg <ridhaanchopra13@gmail.com>
2026-02-27 10:36:52 +0800

    Add files via upload

---
+++ ACP_6.py
@@ -0,0 +1,6 @@
+letter = input("Enter letter to check: ")
+
+if ('a' <= letter <= 'z') or ('A' <= letter <= 'Z'):
+    print("This is in the alphabet")
+else:
+    print("This is not in the alphabet")
