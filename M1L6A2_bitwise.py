d8d59d120cc1286cdf485047a89fa0ec79af8161
ridhaanchopra13-sg <ridhaanchopra13@gmail.com>
2026-02-27 21:35:27 +0800

    Add files via upload

---
+++ M1L6A2_bitwise.py
@@ -0,0 +1,8 @@
+a = 5
+b = -10
+print("left shift of a is", a<<1)
+print("left shift of b is", b<<1)
+c = 8
+d = -21
+print("right shift of c is", c>>1)
+print("right shift of d is", d>>1)
---
+++ M1L6A3_gradingsystem.py
@@ -0,0 +1,48 @@
+english = int(input("Enter mark for English\n"))
+math = int(input("Enter mark for Math\n"))
+science = int(input("Enter mark for Science\n"))
+spanish= int(input("Enter mark for Spanish\n"))
+total = english+spanish+math+science
+avg = total/4
+print("Enter Marks Obtained in 5 Subjects: ")
+
+
+if avg>=91 and avg<=100:
+
+    print("Your Grade is A1")
+
+elif avg>=81 and avg<91:
+
+    print("Your Grade is A2")
+
+elif avg>=71 and avg<81:
+
+    print("Your Grade is B1")
+
+elif avg>=61 and avg<71:
+
+    print("Your Grade is B2")
+
+elif avg>=51 and avg<61:
+
+    print("Your Grade is C1")
+
+elif avg>=41 and avg<51:
+
+    print("Your Grade is C2")
+
+elif avg>=33 and avg<41:
+
+    print("Your Grade is D")
+
+elif avg>=21 and avg<33:
+
+    print("Your Grade is E1")
+
+elif avg>=0 and avg<21:
+
+    print("Your Grade is E2")
+
+else:
+
+    print("Invalid Input!")
