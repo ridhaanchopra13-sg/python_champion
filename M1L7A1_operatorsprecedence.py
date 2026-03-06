500b4e0bce5764c567a8d7978acf413254d40632
ridhaanchopra13-sg <ridhaanchopra13@gmail.com>
2026-03-06 21:33:04 +0800

    Add files via upload

---
+++ M1L7A1_operatorsprecedence.py
@@ -0,0 +1,18 @@
+a = 5
+b = 6
+c = 7
+e = 8
+d = (a+c)*e/b
+print("The value of (a+c)*e/c is", d)
+
+
+
+
+
+
+name = "John" 
+age = 0
+if name == "John" or name == "Alex" and age == 0:
+    print("I am happy")
+else:
+    print("I am sad")
---
+++ M1L7A2_divisibility.py
@@ -0,0 +1,8 @@
+print("Enter the numerator")
+num = int(input())
+print("Enter the denominator")
+den = int(input())
+if num%den == 0:
+    print("Numerator is divisble by denominator")
+else:
+    print("numerator is not divisible by denominator")
