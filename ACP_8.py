480230d49fd18eeb4aeead265fadfa860b210ed0
ridhaanchopra13-sg <ridhaanchopra13@gmail.com>
2026-03-13 10:30:20 +0800

    Add files via upload

---
+++ ACP_8.py
@@ -0,0 +1,10 @@
+a = int(input("Enter value of a: "))
+b = int(input("Enter value of b: "))
+c = int(input("Enter value of c: "))
+print("Before swapping:")
+print("a =", a, "b =", b, "c =", c)
+a = b
+b = c
+c = a
+print("After swapping:")
+print("a =", a, "b =", b, "c =", c)
