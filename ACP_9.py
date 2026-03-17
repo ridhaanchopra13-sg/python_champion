6033e2974e0d64a1f7bd88687f101df195d2a235
ridhaanchopra13-sg <ridhaanchopra13@gmail.com>
2026-03-17 09:44:05 +0800

    Add files via upload

---
+++ ACP_9.py
@@ -0,0 +1,8 @@
+age = int(input(" Please enter your age : "))
+if(age < 10):
+   print("Your too young")
+if(age < 20):
+   if(age > 10 ):
+      print("Your age is the perfect age")
+else:
+   print("Your too old")
