50d76c237759d4d684469ae4024abd3892ade399
ridhaanchopra13-sg <ridhaanchopra13@gmail.com>
2026-03-17 19:38:18 +0800

    Add files via upload

---
+++ M2L9A2_stringreverse.py
@@ -0,0 +1,5 @@
+string = str(input("What is the string: "))
+string2 = ''
+for i in string:
+    string2 = i+string2
+print(string2)
---
+++ M2L9A3_reversenumber.py
@@ -0,0 +1,4 @@
+number = int(input("What is the number: "))
+number2 = ''
+for i in range(number,0,-1):
+    print(i)
