7560680ef7c00cf88a321bccb23e100cc62dc24c
ridhaanchopra13-sg <ridhaanchopra13@gmail.com>
2026-03-20 21:30:18 +0800

    Add files via upload

---
+++ M2L10A1_sumofnaturalnumbers.py
@@ -0,0 +1,7 @@
+n = int(input("Enter a number to do Gauss method till: "))
+sum = 0
+i = 1
+while i<=n:
+    sum+=i
+    i = i+1
+print("Sum of numbers till",n,"is: ",sum)
---
+++ M2L10A2_infinity.py
@@ -0,0 +1,3 @@
+i=0
+while i<=-1:
+    print("I WILL RUN FOREVER & EVER & EVER")
