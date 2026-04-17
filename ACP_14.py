c3f9672be10ba61bd9434768c444026b4169ed8e
ridhaanchopra13-sg <ridhaanchopra13@gmail.com>
2026-04-17 20:26:57 +0800

    Add files via upload

---
+++ ACP_14.py
@@ -0,0 +1,13 @@
+import turtle
+screen = turtle.Screen()
+screen.bgcolor("red")
+pen = turtle.Turtle()
+pen.pensize(3)
+pen.pencolor("blue")
+pen.fillcolor("yellow")
+pen.begin_fill()
+for _ in range:
+    pen.forward(150)
+    pen.right(90)
+pen.end_fill()
+turtle.done()
