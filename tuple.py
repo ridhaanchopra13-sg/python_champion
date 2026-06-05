01cdf2f60d27a4f9213b8ad607fa03201891e8fd
ridhaanchopra13-sg <ridhaanchopra13@gmail.com>
2026-06-05 21:28:32 +0800

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,13 @@
+tuplex = ("tuple",False,3.2,1)
+print(tuplex)
+tuplex = (4,6,8,3,6.9)
+print(tuplex)
+tuplex = tuplex +(9,)
+print(tuplex)
+tuple1 = (50,70,60,55,34,50)
+print(tuple1.count(50))
+tuplex = (8,4,9,5,4,5,7,3)
+_slice = tuplex[3:5]
+print(_slice)
+_slice = tuplex[:6]
+print(_slice)
--- weather.py
+++
@@ -1,6 +0,0 @@
-spring = "autumn"
-autumn = spring
-def weather_condition():
-    print("The weather is pleasant in:", spring)
-    print("The weather is same in:", autumn)
-weather_condition()
