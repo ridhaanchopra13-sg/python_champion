8774aca3b011c1dcb7da7434073feedd72d4d505
ridhaanchopra13-sg <ridhaanchopra13@gmail.com>
2026-06-18 12:35:10 +0800

    Add files via upload

---
+++ ACP_symetricdiff.py
@@ -0,0 +1,6 @@
+Set1 = {'blue', 'green'}
+Set2 = {'blue', 'yellow'}
+print(Set1.symmetric_difference(Set2))
+Set1 = {1, 2, 3, 4, 5}
+Set2 = {1, 5, 6, 7, 8, 9}
+print(Set1.symmetric_difference(Set2))
