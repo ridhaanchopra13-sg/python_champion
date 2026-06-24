07ab3a3d6b13d00d58e153341c5684bb02a07cd5
ridhaanchopra13-sg <ridhaanchopra13@gmail.com>
2026-06-24 18:15:29 +0800

    Add files via upload

---
+++ speed.py
@@ -0,0 +1,7 @@
+class vehichle:
+    def __init__(self,speed,milage):
+        self.speed = speed
+        self.milage = milage
+MHA = vehichle(240,987)
+print("The speed is: ",MHA.speed)
+print("The milage is: ",MHA.milage)
